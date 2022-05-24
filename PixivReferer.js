87a29b453dedbceedae578876f183811cf54170b
WOo0W <24245520+WOo0W@users.noreply.github.com>
2022-05-24 16:33:01 +0800

    Create PixivReferer.js

---
+++ PixivReferer.js
@@ -0,0 +1,10 @@
+if ($request.headers['Referer'].match(/pixiv/)) {
+  $done({})
+} else {
+  $done({
+    headers: {
+      ...$request.headers,
+      Referer: 'https://www.pixiv.net'
+    }
+  })
+}
